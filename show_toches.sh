eb2847d40d8417b3ebd4746d383fbed31e0b12bf
Vishaal Prasad <vishaal56@gmail.com>
2022-02-10 13:55:04 -0800

    adding show touches

---
+++ show_toches.sh
@@ -0,0 +1,6 @@
+
+adb shell content insert --uri content://settings/system --bind name:s:show_touches --bind value:i:1
+
+# disable 
+# adb shell content insert --uri content://settings/system --bind name:s:show_touches --bind value:i:0
+
