fa5c2f4c4490cb4e51289327751cf02e02ef2d45
Vishaal Prasad <vishaal56@gmail.com>
2022-02-20 10:20:27 -0800

    Update show_toches.sh

--- show_toches.sh
+++ show_toches.sh
@@ -4,4 +4,4 @@ adb shell content insert --uri content://settings/system --bind name:s:show_touc
 # disable 
 # adb shell content insert --uri content://settings/system --bind name:s:show_touches --bind value:i:0
 
-@ # https://gist.github.com/Pulimet/5013acf2cd5b28e55036c82c91bd56d8
+# https://gist.github.com/Pulimet/5013acf2cd5b28e55036c82c91bd56d8
