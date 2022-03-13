38c6bf83d881fcef854e15fb7b2da19a5778c955
Vishaal Prasad <vishaal56@gmail.com>
2022-03-13 16:37:23 -0700

    adding driver

--- uninstall_all.sh
+++ uninstall_all.sh
@@ -3,3 +3,4 @@ adb uninstall com.ubercab.presidio.exo
 adb uninstall com.ubercab.rider.internal
 adb uninstall com.ubercab.eats.internal
 adb uninstall com.ubercab.eats.exo
+adb uninstall com.ubercab.driver.exo
