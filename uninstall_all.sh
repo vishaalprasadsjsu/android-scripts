096df58143fa7822e554a8a9085c04e08e4a87c6
Vishaal Prasad <vishaal56@gmail.com>
2022-03-13 19:16:42 -0700

    removing time, adding driver, fixing unlock samsung

--- uninstall_all.sh
+++ uninstall_all.sh
@@ -1,6 +1,10 @@
 
 adb uninstall com.ubercab.presidio.exo
 adb uninstall com.ubercab.rider.internal
+
 adb uninstall com.ubercab.eats.internal
 adb uninstall com.ubercab.eats.exo
+
 adb uninstall com.ubercab.driver.exo
+adb uninstall com.ubercab.driver.internal 
+
--- unlock_samsung.sh
+++ unlock_samsung.sh
@@ -1,1 +1,3 @@
-time (aul ; atx 123456 ; aen)
+adb shell input swipe 500 1100 500 200 200 
+adb shell input text 123456 
+adb shell input keyevent 66 
